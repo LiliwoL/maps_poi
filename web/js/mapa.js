6ab7ba66f2b4bed291ec63a0505fa1b22ae5cd0d
mrlukowski <mlukowski@mixbox.pl>
2017-10-17 16:44:19 +0200

    part 2 ready for ajax feature

--- web/js/mapa.js
+++ web/js/mapa.js
@@ -1,16 +1,131 @@
-document.addEventListener("DOMContentLoaded", function() {
 
-function mapaStart()
-{
-    var wspolrzedne = new google.maps.LatLng(53.41935400090768,14.58160400390625);
-    var opcjeMapy = {
-        zoom: 10,
-        center: wspolrzedne,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-    var mapa = new google.maps.Map(document.getElementById("MAPS"), opcjeMapy);
-}
+document.addEventListener("DOMContentLoaded", function () {
 
-mapaStart();
+    var map;
+    var markers = [];
+
+
+    function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('MAPS'), {
+            zoom: 4,
+            center: uluru
+        });
+//listereny
+
+        map.addListener('click', function (e) {
+            placeMarker(e.latLng, map);
+            //  console.log(markers[markers.length - 1]);
+        });
+
+        var clearButton = document.querySelector('#clear');
+        clearButton.addEventListener('click', function (e) {
+            e.preventDefault();
+
+            clearMarkers();
+
+        });
+
+
+
+
+        // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+            searchBox.setBounds(map.getBounds());
+        });
+
+
+        var markers = [];
+        // Listen for the event fired when the user selects a prediction and retrieve
+        // more details for that place.
+        searchBox.addListener('places_changed', function() {
+            var places = searchBox.getPlaces();
+
+            if (places.length == 0) {
+                return;
+            }
+
+            // Clear out the old markers.
+            markers.forEach(function(marker) {
+                marker.setMap(null);
+            });
+            markers = [];
+
+            // For each place, get the icon, name and location.
+            var bounds = new google.maps.LatLngBounds();
+            places.forEach(function(place) {
+                if (!place.geometry) {
+                    console.log("Returned place contains no geometry");
+                    return;
+                }
+                var icon = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                // Create a marker for each place.
+                markers.push(new google.maps.Marker({
+                    map: map,
+                    icon: icon,
+                    title: place.name,
+                    position: place.geometry.location
+                }));
+
+                if (place.geometry.viewport) {
+                    // Only geocodes have viewport.
+                    bounds.union(place.geometry.viewport);
+                } else {
+                    bounds.extend(place.geometry.location);
+                }
+            });
+            map.fitBounds(bounds);
+        });
+
+
+    }
+
+
+
+    function placeMarker(position, map) {
+        var marker = new google.maps.Marker({
+            position: position,
+            map: map
+        });
+        //map.panTo(position);
+        markers.push(marker);
+        //  markers.push([marker.position.lat() ,marker.position.lng() ]);
+    }
+
+    // Sets the map on all markers in the array.
+    function setMapOnAll(map) {
+        for (var i = 0; i < markers.length; i++) {
+            markers[i].setMap(map);
+        }
+    }
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+        setMapOnAll(null);
+    }
+
+    // Shows any markers currently in the array.
+    function showMarkers() {
+        setMapOnAll(map);
+    }
+
+    // Deletes all markers in the array by removing references to them.
+    function deleteMarkers() {
+        clearMarkers();
+        markers = [];
+    }
+    initMap();
 
 });
